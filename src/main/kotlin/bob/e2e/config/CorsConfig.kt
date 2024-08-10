6d608e42bb2d1fe0fa46fe107f699ffea10538c8
ChangHyunLee <dlckdgus200011@gmail.com>
2024-08-11 01:57:07 +0900

    Add cors to connect with next.js

---
+++ src/main/kotlin/bob/e2e/config/CorsConfig.kt
@@ -0,0 +1,22 @@
+package bob.e2e.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
+class CorsConfig {
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin("http://localhost:3000") // Next.js 개발 서버 주소
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
+    }
+}
